8a921d8054f7855bf4b0f1bd0e2a4062fc6d073d
venbl4 <lebmax2@gmail.com>
2021-08-02 23:52:53 +0700

    Add Ex1, Ex2, Ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -4,32 +4,25 @@ namespace Ex3
 {
     class Program
     {
+        // Maksim Lebedev
+        //Написать программу, которая подсчитывает расстояние между точками с координатами x1, y1 и x2,y2
+        //по формуле r=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2).
+        //Вывести результат, используя спецификатор формата .2f (с двумя знаками после запятой);
         static void Main()
         {
-            string[] str = { "Имя: Вася", "Возраст: 18", "Средний балл по математике: 4" };
-            int width = Console.WindowWidth, height = Console.WindowHeight;
-            Print();
-
-            while (true)
-            {
-                if (width != Console.WindowWidth || height != Console.WindowHeight)
-                {
-                    width = Console.WindowWidth;
-                    height = Console.WindowHeight;
-                    Print();
-                }
-            }
-
-            void Print()
-            {
-                Console.Clear();
-                int top = Console.WindowHeight / 2 - str.Length / 2;
-                for (int i = 0; i < str.Length; i++, top++)
-                {
-                    Console.SetCursorPosition(Console.WindowWidth / 2 - str[i].Length / 2, top);
-                    Console.Write(str[i]);
-                }
-            }
+            Console.WriteLine("Найдем расстояние между двумя точками с координатами x и y\nВведите координаты первой точки: ");
+            Console.Write("Введите x1 ");
+            Double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y1 ");
+            Double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Координаты второй точки: ");
+            Console.Write("Введите x2 ");
+            Double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y2 ");
+            Double y2 = Convert.ToDouble(Console.ReadLine());
+            Double dist = 0;
+            dist = (Double) Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Расстояние между двумя точками = {dist:f2} ");
         }
     }
 }
