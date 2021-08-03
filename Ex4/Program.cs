af1dfc975ef8633c8841ee3e29d4e087721e0114
venbl4 <lebmax2@gmail.com>
2021-08-03 16:12:27 +0700

    Добавил Ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 12;
+            Console.WriteLine($"Было:\nПеременная а = {a}\nПеременная b = {b}");
+            //int c = a;// Используем третью переменную
+            //a = b;
+            //b = c;
+            a = b + a;// Без использования третьей переменной
+            b = b - a;
+            b = -b;
+            a = a - b;
+            Console.WriteLine($"Cтало:\nПеременная а = {a}\nПеременная b = {b}");
+        }
+    }
+}
