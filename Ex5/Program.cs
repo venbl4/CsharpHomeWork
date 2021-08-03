7105d1d9d6683117b82987c9cb86eebbbc2a627b
venbl4 <lebmax2@gmail.com>
2021-08-03 18:17:35 +0700

    Add Ex5

---
+++ Ex5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет! Введите вашу фамилию: ");
+            string surname = Console.ReadLine();
+            Console.Write("Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Место вашего проживания: ");
+            string home = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Фамилия: {surname} \nИмя: { name} \nМесто проживания: { home}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
